0904dc8c174d7144b84a5f8345f368dece902e15
10yard <jon123wilson@hotmail.com>
2024-01-30 21:02:26 +0000

    Almost done with default add-on romlist

--- dk_system.py
+++ dk_system.py
@@ -95,6 +95,7 @@ def read_romlist():
                             des = des.replace("CK ", "# ").replace("CK", "# ")
                             des = des.replace("BK ", "^ ").replace("BK", "^ ")
                             des = des.replace("ZK ", "² ").replace("ZK", "² ")
+                            des = des.replace("JR ", "³ ").replace("JR", "³ ")
                             des = des.replace("1/2", "{ ").replace("1/4", "} ")
 
                             # Assume defaults when not provided
