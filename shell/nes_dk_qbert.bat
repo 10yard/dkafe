73ab6c04dca76af50a7c630536092914a0949a58
10yard <jon123wilson@hotmail.com>
2024-01-31 21:59:29 +0000

    Added more NES romhacks: NES DK Arrietty World NES DK Japan NES DK Luigi Edition NES Q-Bert Kong NES Wario vs DK

---
+++ shell/nes_dk_qbert.bat
@@ -0,0 +1,2 @@
+cd dkwolf
+dkwolf nes -cart "%DKAFE_SHELL_ROMS%\nes\Donkey Kong Qbert.nes" -nofilter %DKAFE_SHELL_VIDEO%
---
+++ shell/nes_dk_wario.bat
@@ -0,0 +1,2 @@
+cd dkwolf
+dkwolf nes -cart "%DKAFE_SHELL_ROMS%\nes\Wario vs Donkey Kong Hack.nes" -nofilter %DKAFE_SHELL_VIDEO%
